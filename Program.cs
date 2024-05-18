f1ca1c2d348e8ce2c28ef75f50df2078f494b3a7
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-18 19:36:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,10 +3,17 @@
 namespace introduce_to_c {
     class Program {
         static void Main() {
-            int number = 100;
+
+            // byte number = 255; 8bit
+            // short number = 32000; 16bit
+            // int number = 100; 32bit
+            long number = 1000; // 64bit
             number = 10;
             Console.WriteLine("Variable: " + number + ".");
-            
+
+            float num = 4.56f;
+            // double num = 44.77d; 2x
+            Console.WriteLine("Variable: " + num + ".");
         }
     }
 }
