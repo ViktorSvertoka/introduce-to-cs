c8f84809184509c4a9f034d62ebdecb746fd5226
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-18 19:25:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,11 +3,10 @@
 namespace introduce_to_c {
     class Program {
         static void Main() {
-            Console.WriteLine("Hello");
-            Console.Write("Hello 1");
-            Console.Write("Hello 2\n");
-            Console.Write("Hello 3\n\n");
-            Console.ReadKey();
+            int number = 100;
+            number = 10;
+            Console.WriteLine("Variable: " + number + ".");
+            
         }
     }
 }
