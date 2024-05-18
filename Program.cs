2dba89126dc6bd7f6bbe9bb8143f7f05fcd48543
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-19 00:05:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
-﻿namespace introduce_to_c {
+﻿using System.Formats.Asn1;
+
+namespace introduce_to_c {
     class Program {
         static void Main() {
 
-            // Console.WriteLine(Math.Round(4.99f));
-            // Console.WriteLine(Math.Ceiling(4.99f));
-            // Console.WriteLine(Math.Floor(4.99f));
+            int a = 5;
 
-            // Console.WriteLine(Math.Min(5, 0));
-            // Console.WriteLine(Math.Max(5, 0));
-
-            Console.WriteLine("Enter the radius of the circle");
-
-            double radius = Convert.ToDouble(Console.ReadLine());
-            
-            double area = Math.PI * Math.Pow(radius, 2);
-
-            Console.WriteLine("The area of a circle with radius {0} is {1}", radius, area);
+            if (a > 7) {
+                Console.WriteLine("Number > 5");
+            } else if (a < 5) {
+                Console.WriteLine("Number < 5");
+            } else {
+                Console.WriteLine("Number is not 5");
+            }
         }
     }
 }
