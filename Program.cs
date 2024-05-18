51813a967465dd12665d7b0c618f2b605ca1ee53
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-18 19:06:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ namespace introduce_to_c {
     class Program {
         static void Main() {
             Console.WriteLine("Hello");
+            Console.Write("Hello 1");
+            Console.Write("Hello 2");
+            Console.Write("Hello 3");
             Console.ReadKey();
         }
     }
