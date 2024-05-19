807162a395550a003c4e96fa09a241a8d8d6f36d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-19 22:23:30 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,61 +1,38 @@
-﻿namespace introduce_to_c {
+﻿using System.Collections.Generic;
+
+namespace introduce_to_c {
     class Program {
         static void Main() {
 
-            // int[] nums = new int[5];
-            // nums[0] = 250;
-            // nums[1] = 200;
-            // nums[2] = 150;
-            // nums[3] = 100;
-            // nums[4] = 50;
+            // short[] nums = { 5, 6, 7, 8, 5, 3, 4 };
 
-            // Console.WriteLine("Elements: " + nums[0]);
-
-            // string[] words = new string[] {"John", "Bob", "Alex"};
-
-            // words[1] = "Josh";
-
-            // for(byte i = 0; i < words.Length; i++) {
-            //     Console.WriteLine("El: " + words[i]);
-
+            // foreach(short el in nums) {
+            //     Console.WriteLine("El: " + el);
             // }
 
-            // short[] numbers = new short[10];
-            // short summa = 0;
+            // short[,] nums = { { 5, 6, 7 }, { 8, 5, 3 }, { 4, 6, 8 } };
 
-            // Random random = new Random();
-
-            // for(byte i = 0; i < numbers.Length; i++) {
-
-            //     numbers[i] = Convert.ToInt16(random.Next(-15, 15));
-
-            //     Console.WriteLine("Elements: " + numbers[i]);
-
-            //     summa += numbers[i];
+            // foreach(short el in nums) {
+            //     Console.WriteLine("El: " + el);
             // }
-            // Console.WriteLine("Summa: " + summa);
 
-            // char[,] symbols = new char[2, 3];
-            // symbols[0, 0] = 'H';
-            // Console.WriteLine(symbols[0, 0]);
-
-            int[,] nums = new int[,] {
-                {4, 6, 7},
-                {5, 7, 3},
-                {3, 3, 2}
+            List<int> numbers = new List<int>(){
+                4, 6, 7
             };
 
-            nums[1, 2] = 56;
+            numbers.Add(40);
+            numbers.Add(100);
+            numbers.Add(5);
 
+            numbers.Remove(100);
 
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(nums[i, j] + "\t");
-                }
-                Console.WriteLine();
+            numbers.Sort();
+            numbers.Reverse();
+
+            foreach(int el in numbers) {
+                Console.WriteLine("El: " + el);
             }
+
 
         }
     }
