b29aab6f7994c40d709b056a716e7f99e9aa208b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-19 20:24:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,50 +1,63 @@
 ﻿namespace introduce_to_c {
     class Program {
         static void Main() {
-            // for(byte i = 0; i <= 10; i++) {
-            //     Console.WriteLine("Element: {0}", i);
+
+            // int[] nums = new int[5];
+            // nums[0] = 250;
+            // nums[1] = 200;
+            // nums[2] = 150;
+            // nums[3] = 100;
+            // nums[4] = 50;
+
+            // Console.WriteLine("Elements: " + nums[0]);
+
+            // string[] words = new string[] {"John", "Bob", "Alex"};
+
+            // words[1] = "Josh";
+
+            // for(byte i = 0; i < words.Length; i++) {
+            //     Console.WriteLine("El: " + words[i]);
+
             // }
 
-            //  for(float i = 200; i > 10; i /= 2) {
-            //     Console.WriteLine("Element: {0}", i);
+            // short[] numbers = new short[10];
+            // short summa = 0;
+
+            // Random random = new Random();
+
+            // for(byte i = 0; i < numbers.Length; i++) {
+
+            //     numbers[i] = Convert.ToInt16(random.Next(-15, 15));
+
+            //     Console.WriteLine("Elements: " + numbers[i]);
+
+            //     summa += numbers[i];
             // }
+            // Console.WriteLine("Summa: " + summa);
 
-            // byte j = 0;
-            // while(j < 10) {
-            //     Console.WriteLine("Element: {0}", j);
-            //     j++;
-            // }
+            // char[,] symbols = new char[2, 3];
+            // symbols[0, 0] = 'H';
+            // Console.WriteLine(symbols[0, 0]);
 
-            // byte i = 10;
-            // while(i >= 1) {
-            //     Console.WriteLine("Element: {0}", i);
-            //     i--;
-            // }
+            int[,] nums = new int[,] {
+                {4, 6, 7},
+                {5, 7, 3},
+                {3, 3, 2}
+            };
 
-            // bool isHasCar = true;
-            // while(isHasCar) {
-            //     string end = Console.ReadLine();
-            //     if(end == "end") {
-            //         isHasCar = false;
-            //     }
-            // }
+            nums[1, 2] = 56;
 
-            // byte i = 100;
 
-            // do
-            // {
-            //     Console.WriteLine("Element: " + i);
-            // } while (i < 10);
-
-            for(short i = 0; i < 10; i++) {
-                // if (i > 5)
-                //     break;
-
-                if (i % 2 == 0)
-                    continue;
-
-                Console.WriteLine("Element: " + i);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(nums[i, j] + "\t");
+                }
+                Console.WriteLine();
             }
+
         }
     }
 }
+
