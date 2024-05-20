63b80aea6bf31084e3820dcc862c32c6132f2965
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-20 18:01:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,16 @@
 namespace introduce_to_c {
     class Program {
         static void Main() {
-           
+            string word = "Hello";
+            word += "!";
+            word = String.Concat(word, "!!");
+            Console.WriteLine(word);
+
+            string people = "Alex,Bob,John";
+            string[] names = people.Split(new char[]{','});
+            foreach(string el in names)
+            Console.WriteLine(el);
+
 
         }
     }
