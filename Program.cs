1e6a850467b6a27931e65aa12d3ab13fa125bd6c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-24 20:17:58 +0300

    Try & Catch

--- Program.cs
+++ Program.cs
@@ -1,26 +1,16 @@
-﻿using System.IO;
+﻿using System;
 
 namespace introduce_to_c {
     class Program {
         static void Main() {
-            Console.WriteLine("Enter text: ");
-            string text = Console.ReadLine();
 
-            using(FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate)) {
-                byte[] array = System.Text.Encoding.Default.GetBytes(text);
-
-                stream.Write(array, 0, array.Length);
-
+            try {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num);
+            } catch {
+                Console.WriteLine("You entered the wrong format!");
             }
-
-            using(FileStream stream1 = File.OpenRead("info.txt")) {
-                byte[] array = new byte[stream1.Length];
-                stream1.Read(array, 0, array.Length);
-
-                string textFromFile = System.Text.Encoding.Default.GetString(array);
-                Console.WriteLine(textFromFile);
-
-            }
+            
         }
     }
 }
