b141cecb5859e5825181a2e14b2046cbb1e25e80
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-24 19:55:32 +0300

    Write & Read from file txt

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
-﻿
+﻿using System.IO;
 
 namespace introduce_to_c {
     class Program {
         static void Main() {
-            string word = "Hello";
-            word += "!";
-            word = String.Concat(word, "!!");
-            Console.WriteLine(word);
+            Console.WriteLine("Enter text: ");
+            string text = Console.ReadLine();
 
-            string people = "Alex,Bob,John";
-            string[] names = people.Split(new char[]{','});
-            foreach(string el in names)
-            Console.WriteLine(el);
+            using(FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate)) {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
 
+                stream.Write(array, 0, array.Length);
 
+            }
+
+            using(FileStream stream1 = File.OpenRead("info.txt")) {
+                byte[] array = new byte[stream1.Length];
+                stream1.Read(array, 0, array.Length);
+
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine(textFromFile);
+
+            }
         }
     }
 }
